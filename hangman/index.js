8f6ccec47f5013b32d4dad5c39d4048ef8cdd5ec
Taffyyy123 <tsolmn9@gmail.com>
2024-08-13 18:07:37 +0800

    js2

--- hangman/index.js
+++ hangman/index.js
@@ -1,5 +1,24 @@
 const body = document.getElementsByTagName("body")[0];
 const alphabet = document.getElementById("alphabet");
+const underline = document.getElementsByClassName("randomWord");
+let selectedLetters = document.getElementById("selectedLetters");
+const categories = [
+  "monkey",
+  "elephant",
+  "horse",
+  "panda",
+  "giraffe",
+  "crocodile",
+];
+const randomWord = categories[Math.floor(Math.random() * categories.length)];
+for (let i = 0; i < randomWord.length; i++) {
+  const replacement = document.createElement("div");
+  replacement.innerHTML = "__";
+  replacement.className = randomWord[i];
+  replacement.className = "randomWord";
+  selectedLetters.appendChild(replacement);
+}
+
 let letters = [
   "a",
   "b",
@@ -28,13 +47,16 @@ let letters = [
   "y",
   "z",
 ];
-let selectedLetters = document.getElementById("selectedLetters");
 
 let lives = document.createElement("div");
 lives.className = "lives";
 let result = 10;
 lives.innerHTML = `You Have ${result} lives`;
 body.appendChild(lives);
+
+function gameOver() {
+  lives.innerHTML = `Game Over Word was: ${randomWord}`;
+}
 
 for (let i = 0; i < letters.length; i++) {
   const container = document.createElement("button");
@@ -46,32 +68,46 @@ for (let i = 0; i < letters.length; i++) {
     const clickedcontainer = document.createElement("div");
     clickedcontainer.innerHTML = letters[i];
     clickedcontainer.className = "letters-container";
-    for (let j = 0; j < randomWord.length; j++) {
-      if (randomWord[j].includes(letters[i])) {
-        for (
-          let n = 0;
-          n < document.getElementsByClassName(randomWord[j]).length;
-          n++
-        ) {
-          document.getElementsByClassName(randomWord[j])[n].innerHTML =
-            randomWord[j];
-        }
-      } else {
-        if (result < 1) {
-          lives.innerHTML = `Game Over Word was: ${randomWord}`;
-        } else {
-          result - 1;
+    if (randomWord.includes(letters[i]) == true) {
+      for (let j = 0; j < randomWord.length; j++) {
+        if (randomWord[j] === letters[i]) {
+          underline[j].innerHTML = letters[i];
         }
       }
+    } else {
+      result--;
+      lives.innerHTML = `You Have ${result} lives`;
+      body.appendChild(lives);
     }
+    setInterval(complete(), 100);
+    setInterval(gameOver(), 100);
   });
   alphabet.appendChild(container);
 }
-
-const randomWord = "fanta";
-for (let i = 0; i < randomWord.length; i++) {
-  const replacement = document.createElement("div");
-  replacement.className = randomWord[i];
-  replacement.id = "randomWord";
-  selectedLetters.appendChild(replacement);
+function complete() {
+  let completed = "";
+  for (let i = 0; i < randomWord.length; i++) {
+    const letter = document.getElementById("selectedLetters").childNodes[i];
+    completed += letter.innerHTML;
+  }
+  if (completed == randomWord) {
+    document.getElementsByClassName("lives");
+    lives.innerHTML = "You Won!";
+    playAgain();
+  }
+}
+function gameOver() {
+  if (result <= 0) {
+    document.getElementsByClassName("lives");
+    lives.innerHTML = `Game Over!`;
+  }
+}
+function playAgain() {
+  const button = document.createElement("button");
+  button.innerHTML = "enter the here play again";
+  document.body.appendChild(button);
+  button.onclick = refresh();
+}
+function refresh() {
+  location.reload();
 }
